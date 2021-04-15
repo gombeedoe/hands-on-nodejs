8a8657fd1c9922df78034d8dd998a7269b5a2d97
Gombee Doe <>
2021-04-15 00:58:19 +0000

    multi-process ipc

--- multi-process/ipc/multi-process.js
+++ multi-process/ipc/multi-process.js
@@ -5,10 +5,12 @@ console.log('メインプロセス', process.pid)
 
 setupMaster({ exec: `${__dirname}/web-app` })
 
-console.log(__dirname)
-
 const cpuCount = require('os').cpus().length
 for (let i = 0; i < cpuCount; i++) {
   const sub = fork()
   console.log('サブプロセス', sub.process.pid)
+  sub.send(3000)
+  sub.on('message', ({ pid, response }) => {
+    console.log(process.pid, `${pid}が${response}を返します`)
+  })
 }
